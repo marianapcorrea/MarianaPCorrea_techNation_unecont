58b7311e6519f3315bd64797343f1b90796bbcd2
Mariana Correa <mpcs270391@gmail.com>
2024-02-16 12:18:44 -0300

    feat: Add functional filter logic for invoice tab

--- src/data/auxiliarData.js
+++ src/data/auxiliarData.js
@@ -2,13 +2,13 @@ export const INDICATORS_FILTERS = ['Tudo', 'Trimestre', 'Mês'];
 
 export const INVOICES_FILTERS = ['Todas', 'Mês de emissão', 'Mês de cobrança', 'Mês de pagamento', 'Status da nota'];
 
-export const MONTHS = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+export const MONTHS = ['--', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
 export const TRIMESTERS = ['1º Trimestre', '2º Trimestre', '3º Trimestre', '4º Trimestre'];
 
 export const YEARS = ['2023', '2022', '2021'];
 
-export const INVOICE_STATUS = ['Emitida', 'Cobrança realizada', 'Pagamento em atraso', 'Pagamento realizado']
+export const INVOICE_STATUS = ['--','Emitida', 'Cobrança realizada', 'Pagamento em atraso', 'Pagamento realizado']
 
 
 export const INADIMPLENCE_EVOLUTION=  {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { INDICATORS_FILTERS, INVOICES_FILTERS, INVOICE_STATUS, MONTHS, TRIMESTERS, YEARS } from "./data/auxiliarData";
 import { getData } from "./data/fetchJson";
+import { clearCards,createOptionElemente, getInvoiceFilterByValue, createInvoiceItem, createNoDataCard, filterByMonth, handleValuesSum } from "./utils";
 
 const navOption = document.querySelectorAll('.navOption')
 const tabs = document.querySelectorAll('.tab')
@@ -9,6 +10,7 @@ const filterIndicatorBy = document.querySelector('#filter-indicators-by')
 const invoiceFilterInput = document.querySelector('#invoice-filter')
 const filterInvoiceBy = document.querySelector('#filter-invoice-by')
 const invoiceDataContainer =document.querySelector("#invoice-data")
+const cardFullValue = document.querySelector('#full-value div span')
 
 
 // Função para adicionar a classe navOptionActive ao clicar em uma opção do menu lateral
@@ -32,15 +34,7 @@ const invoiceSection = document.querySelector('#invoice-section')
 )
 }
 
-//Cria um elemento option para ser adicionado ao filtro
-const createOptionElemente = (value, className) => {
-  const optionEL =document.createElement('option')
-  optionEL.value = value
-  optionEL.innerText = value
-  optionEL.classList.add(className)
-return optionEL
-}
-
+// Função para popular os filtros iniciais
 const handleInitialFiltersPopulation = () => {
   const selectElArray = document.querySelectorAll('select')
   selectElArray.forEach(({id}) => handleFiltersOptions(id))
@@ -93,17 +87,24 @@ const handleFiltersIndicators = ({target}) => {
 }
 
 // Função para adicionar as opções de filtro de acordo com o período selecionado - Notas Fiscais
-const handleFiltersInvoice = ({target}) => {
+const handleFiltersInvoice = ({target},data) => {
   // Remove as opções anteriores
   const options = document.querySelectorAll('#invoice-filter .filter-option')
   options.forEach((option) => option.remove())
 
   switch (target.value) {
+    case "Todas":
+    invoiceFilterInput.appendChild(createOptionElemente("--",'filter-option'))
+    data?.forEach((invoice) => {
+      invoiceDataContainer.appendChild(createInvoiceItem(invoice));
+    })
+    break
+  
     case "Mês de emissão":
       case "Mês de cobrança":
       case "Mês de pagamento": 
     MONTHS.forEach((month) => {
-      invoiceFilterInput.appendChild(createOptionElemente(month,'filter-option'))
+      invoiceFilterInput.appendChild(createOptionElemente((month),'filter-option'))
     })
     break;
 
@@ -115,45 +116,33 @@ const handleFiltersInvoice = ({target}) => {
   }
 }
 
-// Função de geração dinâmica de elementos li para dados de notas fiscais
-const createInvoiceItem = ({payerName, invoiceId, emissionDate, dueDate, paymentDate, invoiceValue, bankDocument, invoiceStatus}) => {
-  const itemLi = document.createElement('li');
-  itemLi.classList.add('invoice-item-data');
+// Função para aplicar o filtro de acordo com o valor selecionado
+const applyFilterInvoice = ({target},data) => {
+  let filteredData = []
+  const filter = getInvoiceFilterByValue(filterInvoiceBy.value)
+if (filter ==='invoiceStatus') {
+  filteredData = data?.filter((invoice) => invoice[filter] === target.value)
+}
+  MONTHS.forEach((month, i) =>{
+  switch (target.value) {
+    case month==="--":
+      data?.forEach((invoice) => {
+        invoiceDataContainer.appendChild(createInvoiceItem(invoice));
+      })
+      case month:
+        filteredData = filterByMonth(data, filter,i)
+    }} )
+    return filteredData.length>0? handleInvoiceCardPopulation(filteredData):  createNoDataCard()
+}
 
-  const div1 = createDiv('Nome do Pagador: ', payerName, 'Número de identificação da nota: ', invoiceId);
-  itemLi.appendChild(div1);
-
-  const div2 = createDiv('Data de emissão', emissionDate, 'Data da cobrança: ', dueDate, 'Data do pagamento: ', paymentDate);
-  itemLi.appendChild(div2);
-
-  const div3 = createDiv('Valor da nota: R$ ', (invoiceValue.toFixed(2)), 'Documento do boleto bancário: ', bankDocument, 'Status da nota: ', invoiceStatus);
-  itemLi.appendChild(div3);
-
-  return itemLi;
-};
-
-const createDiv = (...args) => {
-  const div = document.createElement('div');
-
-  for (let i = 0; i < args.length; i += 2) {
-    const span1 = document.createElement('span');
-    span1.classList.add('title');
-    span1.textContent = args[i];
-    const span2 = document.createElement('span');
-    span2.classList.add('data');
-    span2.textContent = args[i + 1];
-    
-    const spanContainer = document.createElement('span');
-    spanContainer.appendChild(span1);
-    spanContainer.appendChild(span2);
-
-    div.appendChild(spanContainer);
-  }
-
-  return div;
-};
-
-
+// Renderiza  as notas fiscais em elementos li
+const handleInvoiceCardPopulation= (invoices)=>{ 
+  clearCards()
+invoices?.forEach((invoice) => {
+  const invoiceItem = createInvoiceItem(invoice);
+  invoiceDataContainer.appendChild(invoiceItem);
+});
+}
 
 window.onload = async()=> {
   //fetch de dados de notas fiscais (json)
@@ -168,17 +157,16 @@ window.onload = async()=> {
   //Popula os filtros de acordo com as opções disponíveis
   handleInitialFiltersPopulation()
 
-  // Adiciona event listener para o filtro de indicadores
+  // Adiciona event listener para os filtros
   filterIndicatorBy.addEventListener('change', (event)=> handleFiltersIndicators(event))
 
   filterInvoiceBy.addEventListener('change', (event)=> 
-  handleFiltersInvoice(event))
+  handleFiltersInvoice(event, invoices))
 
-  // Renderiza inicialmente as notas fiscais em elementos li
-  invoices?.forEach((invoice) => {
-    const invoiceItem = createInvoiceItem(invoice);
-    invoiceDataContainer.appendChild(invoiceItem);
-  });
+  handleInvoiceCardPopulation(invoices)
+
+  invoiceFilterInput.addEventListener('change', (event) => applyFilterInvoice(event, invoices))
+  cardFullValue.innerHTML =handleValuesSum(invoices).toFixed(2)
   }
 
   /* 
@@ -191,17 +179,14 @@ window.onload = async()=> {
 
     Notas Fiscais:
       - Criar função para lidar com filtros;
-      - Criar função para deixar dinamico a renderização de notas.
-      - Criar notas fiscais falsas para renderizar;
 
     Geral:
+      - Adicionar filtro para ano vigente
       - Estilização de filtros
       - Criar documentação para o projeto, explicando a estrutura, funcionalidades e decisões tomadas;
-      - Modificar package.json para adicionar informações sobre o projeto e pessoa desenvolvedora;
-      - Criar repo public no github;
       - head: trocar icone e titulo;
       - Estilização mobile;
-      - Adicionar icones;
+      - Adicionar icones;applyFilterInvoice
       - Adicionar transições;
       
   */
